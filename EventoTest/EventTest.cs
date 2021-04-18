a36d021681364f8405cd241c0b071fb3095ec0eb
Ze <123321>
2021-04-19 00:53:26 +0100

    EventTest
    teste unitario para criar evento

---
+++ EventoTest/EventTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using API.Models.Evento;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventoTest
+{
+    [TestClass]
+    public class EventTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Evento evento = new Evento();
+
+            evento.nomeEvento = "Evento ";
+            evento.dataEvento = "26/08/2021";
+            evento.localEvento = "Lá";
+            evento.tipoEvento = "Festa";
+            evento.observacoes = "nada";
+
+            Evento.CreateEvento(evento);
+        }
+    }
+}
