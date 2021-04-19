9481bb0c6717aebec219421967a97674c46c7cc4
Ze <123321>
2021-04-19 21:21:36 +0100

    PessoaTest
    Teste unitario a pessoa

---
+++ Pessoastest/PessoaTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using API.Models.Pessoas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pessoastest
+{
+    [TestClass]
+    public class PessoaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Pessoa pessoa = new Pessoa();
+
+
+            pessoa.telefone = "915236985";
+            pessoa.nome = "Zé";
+            pessoa.zona = "Ourem";
+            pessoa.email = "123@hotmail.com";
+            pessoa.nif = "220985468";
+            pessoa.genero = "macho";
+            pessoa.dataNasc = "12/12/2020";
+            //pessoa.comoConheceu 
+
+            pessoa.CreatePessoa(pessoa);
+        }
+    }
+}
---
+++ Pessoastest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pessoastest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pessoastest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eccadc8e-8049-4a6d-9c0a-c3ca0df8be90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
